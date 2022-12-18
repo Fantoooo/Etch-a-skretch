6f4367302d75ee59a805ca865a4443ac18da3a33
Fantoooo <celian.lecorvoisier@orange.fr>
2022-12-18 22:32:29 +0100

    now can change the height

--- javascript.js
+++ javascript.js
@@ -1,15 +1,13 @@
 /*créer le tableau */
-let height = 10;
-
-const container = document.querySelector("#container");
-
-for (let i=0; i<height**2; i++){
-    const item = document.createElement("div");
-    item.classList.add("item")
-    container.append(item)
-    item.addEventListener('mouseover', () => item.style.backgroundColor='grey');
-};
-
+function tableau(height){
+    const container = document.querySelector("#container");
+    for (let i=0; i<height**2; i++){
+        const item = document.createElement("div");
+        item.classList.add("item")
+        container.append(item)
+        item.addEventListener('mouseover', () => item.style.backgroundColor='grey');
+    };
+}
 
 /*changer la taille du tableau*/
 const changeheight = document.querySelector('#changewidtheight');
