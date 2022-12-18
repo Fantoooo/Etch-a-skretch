9e2a2effa60ed3612c9705f3370acb92fd9ae25b
Fantoooo <celian.lecorvoisier@orange.fr>
2022-12-18 21:30:09 +0100

    changements : - récupere cliques sur bouton de changement height - changement du création de tableau en fonction

--- javascript.js
+++ javascript.js
@@ -2,10 +2,18 @@
 let height = 10;
 
 const container = document.querySelector("#container");
+
 for (let i=0; i<height**2; i++){
     const item = document.createElement("div");
     item.classList.add("item")
     container.append(item)
-    console.log(i)
-    item.addEventListener('mouseover', () => item.style.backgroundColor='grey')
+    item.addEventListener('mouseover', () => item.style.backgroundColor='grey');
 };
+
+
+/*changer la taille du tableau*/
+const changeheight = document.querySelector('#changewidtheight');
+changeheight.addEventListener('click', () => {
+    height = Number(prompt("new width :"))
+    alert(height)
+})
