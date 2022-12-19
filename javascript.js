7592e95b94fb304354eb0481f974cd29d68436b0
Fantoooo <celian.lecorvoisier@orange.fr>
2022-12-19 09:50:41 +0100

    now random color mode and eraser are good

--- javascript.js
+++ javascript.js
@@ -7,6 +7,22 @@ function removecontent(id){
             child = container.lastElementChild;
         }
 }
+/*changer la couleur */
+const changecolor = document.querySelector('#changecolor');
+const showcolor = document.querySelector('#showcolor')
+let actualcolor = 'grey'
+changecolor.addEventListener('click', () => {
+    color = prompt("new color : (must be a color in english and javascript friendly)")
+    showcolor.setAttribute('style', `background-color:${color}; box-shadow:0px 0px 45px 2px ${color};`)
+    actualcolor = color
+})
+/*récupere la couleur actuelle */
+function getactualcolor(){
+    let randomlist = ["red","blue","green","darkblue","darkgreen","darkred","aqua"]
+    if(actualcolor == 'random'){return randomlist[Math.floor(Math.random()*randomlist.length)]}
+    if(actualcolor == 'eraser'){return 'white'}
+    return actualcolor
+}
 /*créer le tableau */
 function tableau(height){
     const container = document.querySelector("#container");
@@ -20,11 +36,26 @@ function tableau(height){
         item.classList.add("item")
         item.setAttribute('style', `height:${sizecase}; width: ${sizecase};`)
         container.append(item)
-        console.log(i+1)
-        item.addEventListener('mouseover', () => item.style.backgroundColor='grey');
+        item.addEventListener('mouseover', () => item.style.backgroundColor=getactualcolor());
     };
 }
+/*différents modes de couleur*/
+/*1. rainbow mode */
+const rainbowcolor = document.querySelector('#rainbow')
+rainbowcolor.addEventListener('click', () => {
+    actualcolor = 'random'
+})
+/*2. eraser mode */
+const erasermode = document.querySelector('#eraser')
+erasermode.addEventListener('click', () => {
+    actualcolor = 'eraser'
+})
+
+/*initialise le tableau*/
 tableau(10)
+
+
+
 /*changer la taille du tableau*/
 const changeheight = document.querySelector('#changewidtheight');
 changeheight.addEventListener('click', () => {
